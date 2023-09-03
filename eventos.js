c2add418916deff5d885657f5a224d10d72651ba
constcamila <141197001+constcamila@users.noreply.github.com>
2023-09-03 14:43:09 -0300

    solucion click

--- eventos.js
+++ eventos.js
@@ -1,8 +1,14 @@
 document.addEventListener("DOMContentLoaded", function(){
     
-    const div = document.getElementById("div")
-    
+    const div = document.getElementById("div");
+    const btn = document.getElementById("button");
+
     div.addEventListener("click", ()=> {
         alert("Hola! Soy el div");
+    });
+
+    btn.addEventListener("click", function(event){
+        alert("Hola!");
+        event.stopPropagation();
     });
 });
